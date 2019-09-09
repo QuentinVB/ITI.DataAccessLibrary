56d2692d3d8be047e434ff23e18407904640df9e
Benoit Humblot <benoit.humblot@orange.fr>
2019-09-09 14:47:16 +0200

    Create Model Classes

---
+++ ITI.DataAccessLibrary/Model/ContainerShip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.DataAccessLibrary.Model
+{
+    public class ContainerShip
+    {
+        public int ATISCode { get; set; }
+        public string Name { get; set; }
+        public Harbor Destination { get; set; }
+        public Harbor Origin { get; set; }
+        public List<Container> Cargo { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int Crew { get; set; }
+        public int MaxWeight { get; set; }
+        public int MaxWidth { get; set; }
+        public int MaxLength { get; set; }
+        public int MaxHeight { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+}
