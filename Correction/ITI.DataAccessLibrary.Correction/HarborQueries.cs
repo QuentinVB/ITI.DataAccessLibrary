e28d2d2560f2860b09b9ea4d11167b014b8570cf
Benoit Humblot <benoit.humblot@orange.fr>
2019-09-10 09:22:35 +0200

    Create HarborQueries

--- Correction/ITI.DataAccessLibrary.Correction/HarborQueries.cs
+++ Correction/ITI.DataAccessLibrary.Correction/HarborQueries.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ITI.DataAccessLibrary.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace ITI.DataAccessLibrary.Correction
 {
     public class HarborQueries
     {
+        public List<Harbor> GetAllHarbor()
+        {
+            string query = "SELECT * FROM HARBOR";
+
+            return null;
+        }
+
+        public List<Harbor> GetHarborByCountry()
+        {
+            string query = "SELECT COUNTRY, NAME FROM HARBOUR ORDER BY COUNTRY";
+
+            return null;
+        }
+
+        public void InsertHarbor( Harbor harbor )
+        {
+            string query = "INSERT INTO HARBOR VALUES(" +
+                $"{harbor.Id}, " +
+                $"{harbor.Name}, " +
+                $"{harbor.LocalName}, " +
+                $"{harbor.Country}, " +
+                $"{harbor.Latitude}, " +
+                $"{harbor.Longitude}, " +
+                ")";
+        }
     }
 }
