f7e63db6519d2ab7c1829c3b88dc6763986a0827
Quentin Vanbutsele <vanbutsele@intechinfo.fr>
2020-01-28 10:24:24 +0100

    Change target to correction ; replace Container MaxWeigth by Container LoadWeigth

--- ITI.DataAccessLibrary.Tests/HarborTests.cs
+++ ITI.DataAccessLibrary.Tests/HarborTests.cs
@@ -1,8 +1,8 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using ITI.DataAccessLibrary;
-using ITI.DataAccessLibrary.Model;
+using ITI.DataAccessLibrary.Correction;
+using ITI.DataAccessLibrary.Correction.Model;
 
 namespace ITI.DataAccessLibrary.Tests
 {
